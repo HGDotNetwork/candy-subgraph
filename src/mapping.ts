64c8eeb7ba9a4dd66ed92b7e035d9e339fbc0ead
hypergraphdev <service@hg.network>
2021-04-25 14:52:19 +0800

    update code for example

--- src/mapping.ts
+++ src/mapping.ts
@@ -8,22 +8,27 @@ import {
   ClaimedPacketTokens,
   OwnershipTransferred
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { PackageEntity, PackageToken } from "../generated/schema"
 
 export function handlePacketstarted(event: Packetstarted): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = PackageEntity.load(event.transaction.hash.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new PackageEntity(event.transaction.hash.toHex())
 
     // Entity fields can be set using simple assignments
     entity.count = BigInt.fromI32(0)
   }
-
+  let tokenEntity = PackageToken.load(event.params.tokenAddress.toHex())
+  if(tokenEntity == null){
+    tokenEntity = new PackageToken(event.params.tokenAddress.toHex())
+    tokenEntity.total = BigInt.fromI32(0);
+  }
+  tokenEntity.total = tokenEntity.total.plus(event.params.total)
   // BigInt and BigDecimal math are supported
   entity.count = entity.count + BigInt.fromI32(1)
 
@@ -33,6 +38,9 @@ export function handlePacketstarted(event: Packetstarted): void {
 
   // Entities can be written to the store with `.save()`
   entity.save()
+  tokenEntity.save()
+
+
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
